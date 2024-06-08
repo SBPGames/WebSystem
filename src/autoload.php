44fae497cc87bd8baca876a46d9f991b33dbcbfb
Xibitol <xibitol@pimous.dev>
2024-06-09 01:37:35 +0200

    Upgraded doc of loadclass func of the autoloader.

--- src/autoload.php
+++ src/autoload.php
@@ -4,7 +4,6 @@ const PROJECT_VENDOR = "SBPGames";
 const SOURCES_FOLDER = "src";
 const LIBRARIES_FOLDER = "lib";
 
-// TODO: Make it a preprocessing operation (Like in composer).
 /** Retrieves all folder names in the librairies folder (LIBRARIES_FOLDER) if
  * exists, searched relatively from the given `$path`, and if a namespace's
  * vendor name (PHP, PSR-1 and PSR-4). **These folders should be treated as
@@ -32,16 +31,23 @@ function getLibrairiesVendors(string $path): array{
 /** Loads once a file (Like `require_once` do) containing the class called
  * `$classname`. The file is searched relatively from the given `$path` in the
  * sources folder (SOURCES_FOLDER) - if the class is not a library (having
- * PROJECT_VENDOR at namespace's top-level) - or in the librairies folder
+ * PROJECT_VENDOR vendor at namespace's top-level) - or in the librairies folder
  * (LIBRARIES_FOLDER).
+ * 
+ * **NOTE:** This function lets `require_once` throw warnings; otherwise its
+ * final E_COMPILE_ERROR shouldn't be thrown because enough verifications are
+ * made.
  *
  * @param string $classname The name of the class to be loaded, indicating by
  * its namespace the path of its file relative to one of the specific folders.
  * @param string $path A path from where specific folders, where the file is
  * searched, can be found.
  * @return void
- * @throws UnexpectedValueException If `$classname` doesn't have a (valid)
- * vendor name as namespace's top-level (PHP, PSR-1 and PSR-4).
+ * @throws UnexpectedValueException
+ * 		- If `$classname` doesn't have a (valid) vendor name as namespace's
+ * 		top-level (PHP, PSR-1 and PSR-4);
+ * 		- Or if any file containing the class called `$classname` can't be found
+ *		in one of the specific folders.
  */
 function loadClass(string $classname, string $path): void{
 
