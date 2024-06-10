37dee0ef01f2adc575f18d25073c72db8b6af82d
Xibitol <xibitol@pimous.dev>
2024-06-11 00:12:28 +0200

    Implemented Autoloader with spl functions and added global constants.

--- ressource/public/index.php
+++ ressource/public/index.php
@@ -1,71 +1,11 @@
 <?php
-	$words = [
-		"pretty", "handsome", "beautiful", "amazing", "elegant", "cute",
-		"gorgeous", "lovely"
-	];
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>SBP Games</title>
+const PROJECT_PATH = "../..";
+const DEVELOPMENT_MODE = true;
 
-	<link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="32x32">
-	<link rel="apple-touch-icon" href="/favicon.ico">
-	<link rel="apple-touch-startup-image" href="/favicon.ico">
+// Adding a classes autoloader.
+require PROJECT_PATH."/src/autoload.php";
 
-	<style>
-		*{
-			text-align: center;
-
-			box-sizing: border-box;
-		}
-
-		body{
-			margin-inline: 0;
-			width: 100%;
-			padding-inline: 1rem;
-		}
-
-		h1{
-			color: #a33e18;
-		}
-
-		footer{
-			position: absolute;
-
-			bottom: 0;
-			width: calc(100% - 1rem*2);
-		}
-	</style>
-</head>
-<body>
-	<main>
-		<hgroup>
-			<h1>SBP Games</h1>
-			<p><i>
-				A game development studio instantiated by a team of small
-				beautiful prawns!
-			</i></p>
-		</hgroup>
-
-		<p>
-			This page will soon be
-			<strong><?= $words[random_int(0, sizeof($words) - 1)] ?></strong>!
-			Follow us on Github at
-			<a href="https://www.github.com/SBPGames">SBP Games organization</a>.
-		</p>
-
-		<p style="color: white;">I love you my girlfriend - Xibitol</p>
-	</main>
-
-	<footer>
-		<h5>
-			Copyright &copy; 2024 - SBP Games | All the code of this website
-			can be found at <a href="https://www.github.com/SBPGames/WebSystem">the WebSystem git repo</a>.
-		</h5>
-	</footer>
-</body>
-</html>
+spl_autoload_register(function(string $class){
+	\SBPGames\Autoloader\loadClass($class, PROJECT_PATH, DEVELOPMENT_MODE);
+});
