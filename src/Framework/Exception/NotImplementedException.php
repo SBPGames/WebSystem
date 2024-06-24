794783b6c660ea6bf162433754ead4b0a7e10011
Xibitol <xibitol@pimous.dev>
2024-06-24 12:16:14 +0200

    Added exception for not implementend methods.

---
+++ src/Framework/Exception/NotImplementedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SBPGames\Framework\Exception;
+
+use BadMethodCallException;
+
+/**
+ * @package SBPGames\Framework\Exception
+ * @author Xibitol <contact@pimous.dev>
+ */
+class NotImplementedException extends BadMethodCallException{
+
+}
