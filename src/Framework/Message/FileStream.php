e65c289aa4e382a0d3d9cbfbb11dd0ff51a9533f
Xibitol <xibitol@pimous.dev>
2024-12-20 14:43:15 +0100

    Implemented StreamInterface of PSR-7 as a file stream.

---
+++ src/Framework/Message/FileStream.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace SBPGames\Framework\Message;
+
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * @package SBPGames\Framework\Message
+ * @author Xibitol <contact@pimous.dev>
+ */
+class FileStream implements StreamInterface{
+
+	private $stream;
+
+	public function __construct(string $filename, string $mode = ""){
+		$this->open($filename, $mode);
+	}
+
+	// CONSTRUCTORS
+	public static function fromInput(){ return new static("php://input"); }
+	public static function fromOutput(){ return new static("php://output"); }
+
+	// GETTERS
+	private function getStream(){ return $this->stream; }
+
+	public function getSize(): ?int{
+		$size = null;
+
+		if(is_resource($this->getStream())
+			&& !is_bool($stat = fstat($this->getStream()))
+		)
+			$size = $stat["size"];
+
+		return $size;
+	}
+
+	public function getMetadata(?string $key = null): mixed{
+		$metadata = stream_get_meta_data($this->getStream());
+		return isset($key) ? $metadata[$key] : $metadata;
+	}
+	public function isSeekable(): bool{
+		return $this->getMetadata()["seekable"];
+	}
+	public function isReadable(): bool{
+		return preg_match("/r|[waxc]\\+/", $this->getMetadata()["mode"]);
+	}
+	public function isWritable(): bool{
+		return preg_match("/[waxc]|r\\+/", $this->getMetadata()["mode"]);
+	}
+
+	public function tell(): int{
+		if(is_bool($pos = ftell($this->getStream())))
+			throw new \RuntimeException(
+				"Cannot tell current position of the cursor in the stream."
+			);
+
+		return $pos;
+	}
+	public function eof(): bool{
+		return feof($this->getStream());
+	}
+
+	// SETTERS
+	private function open(string $filename, string $mode = ""): void{
+		$s = fopen($filename, $mode);
+
+		if(is_bool($s))
+			throw new \RuntimeException(
+				"Cannot open file $filename with mode $mode."
+			);
+
+		$this->stream = $s;
+	}
+
+	public function detach(){
+		$s = $this->getStream();
+		$this->stream = null;
+		return $s;
+	}
+	public function close(): void{
+		fclose($this->detach());
+	}
+
+	// FUNCTIONS
+	public function seek(int $offset, int $whence = SEEK_SET): void{
+		if(fseek($this->getStream(), $offset, $whence) === -1)
+			throw new \RuntimeException(
+				"Cannot seek along the stream."
+			);
+	}
+	public function rewind(): void{
+		if(!rewind($this->getStream()))
+			throw new \RuntimeException(
+				"Cannot rewind back to the stream's beginning."
+			);
+	}
+
+	public function read(int $length = 2048): string{
+		if(is_bool($read = fread($this->getStream(), $length)))
+			throw new \RuntimeException("Cannot read from stream.");
+
+		return $read;
+	}
+	public function write(string $string): int{
+		if(is_bool($written = fwrite($this->getStream(), $string)))
+			throw new \RuntimeException("Cannot write to stream.");
+
+		return $written;
+	}
+	public function getContents(): string{
+		$buffer = "";
+
+		while(!$this->eof()) $buffer .= $this->read();
+
+		return $buffer;
+	}
+
+	public function __toString(): string{
+		try{
+			$this->rewind();
+			return $this->getContents();
+		}catch(\RuntimeException $_){
+			return "";
+		}
+	}
+}
