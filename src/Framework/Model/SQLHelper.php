0c51c94c2a882af9bcc37e7ba414c0238a48c61c
Xibitol <xibitol@pimous.dev>
2025-01-09 09:20:17 +0100

    Refactored string formatting of SQL helper.

--- src/Framework/Model/SQLHelper.php
+++ src/Framework/Model/SQLHelper.php
@@ -11,10 +11,18 @@ class SQLHelper{
 	private const SELECT_FORMAT = "SELECT DISTINCT * FROM %s %s %s LIMIT %u, %u;";
 	private const WHERE_CLAUSE_FORMAT = "WHERE %s";
 	private const ORDERBY_CLAUSE_FORMAT = "ORDER BY %s";
+	private const ORDERBY_ATOM_FORMAT = "%s %s";
+	private const ORDERBY_ASC = "ASC";
+	private const ORDERBY_DESC = "DESC";
 
 	private const INSERT_FORMAT = "INSERT INTO %s(%s) VALUES (%s) RETURNING *;";
 	
 	private const UPDATE_FORMAT = "UPDATE %s SET %s WHERE %s;";
+
+	private const PREPARED_FORMAT = ":%s";
+	private const PREPARED_ASSOC_FORMAT = "%1\$s = :%1\$s";
+	private const COMMA_SEPARATOR = ", ";
+	private const AND_SEPARATOR = " AND ";
 
 	private string $tableName;
 
@@ -32,9 +40,9 @@ class SQLHelper{
 		$filtersPart = "";
 		if(count($filters) > 0){
 			$filtersPart = sprintf(static::WHERE_CLAUSE_FORMAT,
-				implode(", ", array_map(
+				implode(self::AND_SEPARATOR, array_map(
 					function(string $k): string{
-						return sprintf("%1\$s = :%1\$s", $k);
+						return sprintf(self::PREPARED_ASSOC_FORMAT, $k);
 					}, array_keys($filters)
 				))
 			);
@@ -43,9 +51,11 @@ class SQLHelper{
 		$sortKeysPart = "";
 		if(count($sortKeys) > 0)
 			$sortKeysPart = sprintf(static::ORDERBY_CLAUSE_FORMAT,
-				implode(", ", array_map(
+				implode(self::COMMA_SEPARATOR, array_map(
 					function(string $k, bool $isDesc): string{
-						return sprintf("%s %s", $k, $isDesc ? "DESC" : "ASC");
+						return sprintf(self::ORDERBY_ATOM_FORMAT,
+							$k, $isDesc ? self::ORDERBY_DESC : self::ORDERBY_ASC
+						);
 					}, array_keys($sortKeys), $sortKeys
 				))
 			);
@@ -61,10 +71,10 @@ class SQLHelper{
 	public function generateInsert(array $columns): string{
 		return sprintf(static::INSERT_FORMAT,
 			$this->getTableName(),
-			implode(", ", $columns),
-			implode(", ", array_map(
+			implode(self::COMMA_SEPARATOR, $columns),
+			implode(self::COMMA_SEPARATOR, array_map(
 				function(string $k): string{
-					return sprintf(":%s", $k);
+					return sprintf(self::PREPARED_FORMAT, $k);
 				}, $columns
 			))
 		);
@@ -77,14 +87,14 @@ class SQLHelper{
 	public function generateUpdate(array $columns, array $identifiers): string{
 		return sprintf(static::UPDATE_FORMAT,
 			$this->getTableName(),
-			implode(", ", array_map(
+			implode(self::COMMA_SEPARATOR, array_map(
 				function(string $k): string{
-					return sprintf("%1\$s = :%1\$s", $k);
+					return sprintf(self::PREPARED_ASSOC_FORMAT, $k);
 				}, $columns
 			)),
-			implode(", ", array_map(
+			implode(self::AND_SEPARATOR, array_map(
 				function(string $k): string{
-					return sprintf("%1\$s = :%1\$s", $k);
+					return sprintf(self::PREPARED_ASSOC_FORMAT, $k);
 				}, $identifiers
 			)),
 		);
